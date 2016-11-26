3fcea9303ba1a7fd359e1051a05bf7d1bafd904d
twillzy <william.tanzy@gmail.com>
2016-11-26 16:30:16 +1100

    socket io

--- app.js
+++ app.js
@@ -1,54 +1,51 @@
-var express = require('express.io');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+"use-strict";
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-app.io.route('orders', {
-  create: function(req) {
-    req.io.emit('talk', {
-      message: 'yo sreejo',
-    });
-  }
+// Run server to listen on port 3000.
+const server = app.listen(3000, () => {
+  console.log('listening on *:3000');
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const io = require('socket.io')(server);
+
+app.use(bodyParser.urlencoded({ extended: false } ));
+app.use(express.static('static'));
+
+// Set socket.io listeners.
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Set Express routes.
+app.get('/', (req, res) => {
+  res.json({id: '1'});
 });
 
-module.exports = app;
+app.post('/events', (req, res) => {
+  let to = req.body.To;
+  let fromNumber = req.body.From;
+  let callStatus = req.body.CallStatus;
+  let callSid = req.body.CallSid;
+
+  io.emit('call progress event', { to, fromNumber, callStatus, callSid });
+
+  console.log(to, fromNumber, callStatus, callSid);
+  res.send('Event received');
+});
+
+app.post('/voice', (req, res) => {
+  let twiml = new twilio.TwimlResponse();
+  // Talk in a robot voice over the phone.
+  twiml.say('Call progress events are rad');
+  // Set the response type as XML.
+  res.header('Content-Type', 'text/xml');
+  // Send the TwiML as the response.
+  res.send(twiml.toString());
+});
