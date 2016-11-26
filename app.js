12e32123a2f8326e0b69ad9798545bf15403b405
twillzy <william.tanzy@gmail.com>
2016-11-26 16:33:21 +1100

    socket io

--- app.js
+++ app.js
@@ -1,51 +1,21 @@
-"use-strict";
+'use strict';
+
 const express = require('express');
-const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
+const path = require('path');
 
-const app = express();
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-// Run server to listen on port 3000.
-const server = app.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = require('socket.io')(server);
+const io = socketIO(server);
 
-app.use(bodyParser.urlencoded({ extended: false } ));
-app.use(express.static('static'));
-
-// Set socket.io listeners.
 io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-// Set Express routes.
-app.get('/', (req, res) => {
-  res.json({id: '1'});
-});
-
-app.post('/events', (req, res) => {
-  let to = req.body.To;
-  let fromNumber = req.body.From;
-  let callStatus = req.body.CallStatus;
-  let callSid = req.body.CallSid;
-
-  io.emit('call progress event', { to, fromNumber, callStatus, callSid });
-
-  console.log(to, fromNumber, callStatus, callSid);
-  res.send('Event received');
-});
-
-app.post('/voice', (req, res) => {
-  let twiml = new twilio.TwimlResponse();
-  // Talk in a robot voice over the phone.
-  twiml.say('Call progress events are rad');
-  // Set the response type as XML.
-  res.header('Content-Type', 'text/xml');
-  // Send the TwiML as the response.
-  res.send(twiml.toString());
-});
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
